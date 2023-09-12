93525316f8fa0fc49ebb95ef06321b38c49c3bfb
dev691 <76876210+dev691@users.noreply.github.com>
2023-09-12 14:40:32 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -8,8 +8,8 @@ with open('scraped_Dorks.txt', 'r') as file:
     # Read the content of the file
     file_content = file.read()
 
-    # Use re.sub to replace everything from the first & or ? (whichever comes first)
-    modified_content = re.sub(url_pattern, lambda x: re.sub(r'[&?].*', '', x.group()), file_content)
+    # Use re.sub to replace URLs containing "accounts.google.com" and everything from the first ? or & (whichever comes first) with an empty string
+    modified_content = re.sub(url_pattern, lambda x: '' if 'accounts.google.com' in x.group() else re.sub(r'[?&].*', '', x.group()), file_content)
 
 # Write the modified content back to the file
 with open('scrapped.txt', 'w') as file:
